a1f17714578bc56e50debea5d7afed283ea91273
Philippe Jallet <philippe.jallet.external@atos.net>
2017-03-31 14:34:52 +0200

    Correction appel ressource

--- src/main/resources/public/js/controller.js
+++ src/main/resources/public/js/controller.js
@@ -12,7 +12,7 @@ function MediacentreController($scope, template, model, route, $location) {
 
         model.getStructures(model.me.userId, function(result) {
             $scope.schools = result.structures;
-            if( $scope.schools.size == 1){
+            if( $scope.schools.length == 1){
                 $scope.display.listSchools = false;
                 getRessources($scope.schools[0].UAI);
             } else {
